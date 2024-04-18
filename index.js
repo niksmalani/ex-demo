c597dc994c6a6f40b5edd97a7ca4e4ad4a66f1f6
Niks <niks@192.168.1.7>
2024-04-18 10:41:36 +0530

    done2

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
  const express = require('express'); 
  const app = express()
 
-app.get("/home",(req,res)=>{
-  res.send({message:"home page"})
+app.get("/home",async (req,res)=>{
+
+  const response = await fetch("https://fakestoreapi.com/products");
+  const products = await response.json();
+  res.send({products:products})
 })
 
  app.listen(3000)
